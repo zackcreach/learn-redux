b3b86a9caf00c14758ae58fc34344ce5cb121cc6
zackcreach <zack@peachdish.com>
2017-06-26 18:18:41 -0400

    Add actions for adding/removing hobbies and movies.

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,7 +2,15 @@ var redux = require('redux');
 
 console.log('Starting redux example');
 
-var reducer = (state = {name: 'Anonymous'}, action) => {
+var stateDefault = {
+  name: 'Anonymous',
+  hobbies: [],
+  movies: []
+}
+
+var nextHobbyId = 1;
+var nextMovieId = 1;
+var reducer = (state = stateDefault, action) => {
   // state = state || {name: 'Anonymous'};
 
   switch (action.type) {
@@ -11,6 +19,39 @@ var reducer = (state = {name: 'Anonymous'}, action) => {
         ...state,
         name: action.name
       };
+    case 'ADD_HOBBY':
+      return {
+        ...state,
+        hobbies: [
+          ...state.hobbies,
+          {
+            id: nextHobbyId++,
+            hobby: action.hobby
+          }
+        ]
+      };
+    case 'REMOVE_HOBBY':
+      return {
+        ...state,
+        hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id)
+      };
+    case 'ADD_MOVIE':
+      return {
+        ...state,
+        movies: [
+          ...state.movies,
+          {
+            id: nextMovieId++,
+            title: action.title,
+            genre: action.genre
+          }
+        ]
+      };
+    case 'REMOVE_MOVIE':
+      return {
+        ...state,
+        movies: state.movies.filter((movie) => movie.id !== action.id)
+      }
     default:
       return state;
   }
@@ -25,6 +66,8 @@ var unsubscribe = store.subscribe(() => {
 
   console.log('Name is', state.name);
   document.getElementById('app').innerHTML = state.name;
+
+  console.log('New state', store.getState());
 });
 // unsubscribe();
 
@@ -34,6 +77,44 @@ console.log('currentState', currentState);
 store.dispatch({
   type: 'CHANGE_NAME',
   name: 'Zack'
+});
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Cycling'
+});
+
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'Walking'
+});
+
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 2
+})
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Terminator',
+  genre: 'Action'
+});
+
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id: 1
+})
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Jurassic Park',
+  genre: 'Action'
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Stepbrothers',
+  genre: 'Comedy'
 });
 
 store.dispatch({
